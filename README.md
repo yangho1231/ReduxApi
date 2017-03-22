4661e0ca97948097d1ab94d9b03bccc76cb0278c
yangho1231 <yangho1231@gmail.com>
2017-03-22 03:19:53 -0600

    Created state in searchbar.js

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Youtube API Search with React.
 
 Tutorial from https://github.com/StephenGrider
 
-1)Install Youtube Search API (npm install --save youtube-api-search)
+1) Install Youtube Search API (npm install --save youtube-api-search)
+2) Whenver state changes it re-renders.
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+//{Component} is same as const Component = React.Component;
+//{Component} pulls the property Component
+//ES6 when you have single line you can take out () when creating function.
+//Functional base doesn't have state only Class based.
+//super is used to call parent class.
+class SearchBar extends Component { 
+    constructor(props) {
+        super(props);
 
-const SearchBar = () => {
-    return <input />
+        this.state = { term: '' };
+    }
+    render() {
+        return (
+            <div>
+                <input onChange={event => console.log(event.target.value)} />
+            </div>
+        );
+    }
+
 }
 export default SearchBar;
