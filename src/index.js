9524068e6a4dce5af33cabf2327fd6a453893c59
yangho1231 <yangho1231@gmail.com>
2017-03-22 20:15:40 -0600

    Got the video_List and item show up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import config from '../config.js';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 const API_KEY = config.API_KEY;
 
-const App = () => {
-  return (
-        <div>
-          <SearchBar />
-        </div>
-  );
-  
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+    YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+      this.setState({videos});
+})
+  }
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
